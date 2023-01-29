64ae0a6baea9b020aa67a7af546235898e72523d
Kiel1196 <1kielleik1@gmail.com>
2023-01-29 13:54:40 +0700

    presistent layout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {Link} from '@inertiajs/vue3'
+import {ref} from 'vue'
+
+const timer = ref( 0 )
+
+setInterval( () => timer.value++, 1000 )
+
+defineProps({
+    message: String
+})
+</script>
+
+<template>
+    <Link href="/">Main Page</Link>&nbsp;
+    <Link href="/hello">Show Page</Link>
+    <div>Timer : {{ timer }}</div>
+    <slot></slot>
+</template>
